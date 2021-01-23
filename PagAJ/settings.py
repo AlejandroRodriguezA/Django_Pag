bbc59e6710c3ec6c31bf06f79efba563c5d4c040
Alejandro <rodriguezalvarez@gmail.com>
2021-01-23 11:32:32 +0100

    Agregando procfile, requirement, runtime

--- PagAJ/settings.py
+++ PagAJ/settings.py
@@ -10,6 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
+import dj_database_url
+from decouple import config
+
+
 from pathlib import Path
 from django.core.mail import send_mail
 
@@ -45,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -158,6 +164,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Envio de Email
 
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn PagAJ.wsgi --log-file -
