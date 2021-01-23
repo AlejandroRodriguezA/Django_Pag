90c4f44ff52e5be26916755c1ccdffbc1507f7a9
Alejandro <rodriguezalvarez@gmail.com>
2021-01-23 12:50:33 +0100

    Cambio de BASE_DIR

--- PagAJ/settings.py
+++ PagAJ/settings.py
@@ -19,7 +19,8 @@ from pathlib import Path
 from django.core.mail import send_mail
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
