29a22acdf4b04d3bbf7a932198916629e66719d8
Alejandro <rodriguezalvarez@gmail.com>
2021-01-23 14:30:55 +0100

    fix staticfiles name in static_root

--- PagAJ/settings.py
+++ PagAJ/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'stacticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
