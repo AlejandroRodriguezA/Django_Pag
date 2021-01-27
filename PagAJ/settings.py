ac56c00c2b09d3776a28e282de25b3fc208e03e1
Alejandro <rodriguezalvarez@gmail.com>
2021-01-27 18:07:00 +0100

    STATICFILES_DIRS 2

--- PagAJ/settings.py
+++ PagAJ/settings.py
@@ -180,7 +180,7 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static'),
    ]
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Envio de Email
