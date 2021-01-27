0764b198c472c5c0b65445a9911891f358494585
Alejandro <rodriguezalvarez@gmail.com>
2021-01-27 18:36:35 +0100

    code

--- PagAJ/settings.py
+++ PagAJ/settings.py
@@ -36,6 +36,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['https://aj-ra-l.herokuapp.com/']
 
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+
+
 
 # Application definition
 
@@ -172,15 +182,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-   ]
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
