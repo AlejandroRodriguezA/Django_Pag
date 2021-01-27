0e34c880736e1a5aa8c27f885bc82ad03f620f78
Alejandro <rodriguezalvarez@gmail.com>
2021-01-27 19:26:46 +0100

    code

--- PagAJ/settings.py
+++ PagAJ/settings.py
@@ -77,8 +77,8 @@ ROOT_URLCONF = 'PagAJ.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '~/PagAJ/AJ/templates')],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'AJ/templates')],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
