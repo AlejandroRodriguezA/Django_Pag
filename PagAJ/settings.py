c664bffd013560dca5741c781c49d103f58919c6
Alejandro <rodriguezalvarez@gmail.com>
2021-01-27 16:35:22 +0100

    code

--- PagAJ/settings.py
+++ PagAJ/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
 import django_heroku
+import os
+
 #import dj_database_url
 #from decouple import config
 
