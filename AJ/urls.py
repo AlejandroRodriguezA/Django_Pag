89b572e78dca8ec0608dc68ab32035a9fe9a0cb6
Alejandro <rodriguezalvarez@gmail.com>
2021-01-27 22:07:56 +0100

    add app_name to AJ 3

--- AJ/urls.py
+++ AJ/urls.py
@@ -12,6 +12,8 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+    Importante incluir el app_name
 """
 from django.urls import path
 
